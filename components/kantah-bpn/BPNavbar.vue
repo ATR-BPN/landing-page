1f7a014c2b46a1bb9496f88ffbf31f1b6039585b
Dhea Aria Rachman <dheariarachman@gmail.com>
2021-08-16 17:49:07 +0700

    update navbar

--- components/kantah-bpn/BPNavbar.vue
+++ components/kantah-bpn/BPNavbar.vue
@@ -14,13 +14,13 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
+        <b-navbar-nav class="ml-auto d-flex align-items-center">
           <b-nav-item href="#about-bpn" class="mx-2">Profile</b-nav-item>
           <b-nav-item href="#about-bpn-people" class="mx-2">
             Pelaksana
           </b-nav-item>
-          <b-nav-item-dropdown text="Pelayanan" right>
-            <b-dropdown-item href="/pasti" target="_blank">
+          <b-nav-item-dropdown text="Layanan" right>
+            <b-dropdown-item href="/layanan" target="_blank">
               PASTI
             </b-dropdown-item>
             <b-dropdown-item href="http://curhatbpn-kotabandung.id/">
@@ -30,7 +30,15 @@
             <b-dropdown-item href="#">Maklumat Pelayanan</b-dropdown-item>
           </b-nav-item-dropdown>
           <b-nav-item href="#contact" class="mx-2">Pengaduan</b-nav-item>
-          <b-nav-item href="#" class="mx-2">Penilaian Kinerja</b-nav-item>
+          <b-nav-item
+            href="https://layanan.pastibpn.id/"
+            class="mx-2"
+            target="_blank"
+          >
+            <b-button variant="primary" class="px-4 rounded-pill">
+              Ajukan Permohonan
+            </b-button>
+          </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </div>
